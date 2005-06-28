27cb052451cb0827ec2d5cedab63b2fec9ac59a0
IOhannes m zmölnig <zmoelnig@iem.at>
2005-06-28 16:01:20 +0000

    use $PD

--- lib/start.sh
+++ lib/start.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+if [ "x$PD" = "x" ]
+then
+ PD=pd
+fi
+  
 
-PDPATHS="-path .:abs/:libs/:plugins/:GUI/:DSP/:GUI/abs:DSP/abs"
+PDPATHS="-path .:abs/:libs/:plugins/:GUI/:DSP/:GUI/abs:DSP/abs:libs/iemabs:libs/zexyabs"
 PDLIBS="-lib zexy:iemlib1:iemlib2:iemgui:iemmatrix:iem_ambi"
 
-pd $PDPATHS $PDLIBS $@
+${PD} $PDPATHS $PDLIBS $@
