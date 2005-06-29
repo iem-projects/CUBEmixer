7e1050ecdfdf1f666975386285be2ab97d834281
IOhannes m zmölnig <zmoelnig@iem.at>
2005-06-29 19:30:13 +0000

    more paths

--- lib/DSP/start.sh
+++ lib/DSP/start.sh
@@ -5,7 +5,7 @@ then
   PD=pd
 fi
 
-PDPATHS="-path .:..:../abs/:../libs/:../plugins/:abs"
+PDPATHS="-path .:..:../abs/:../libs/iemabs:../libs/zexyabs:../libs/:../plugins/:abs"
 PDLIBS="-lib zexy:iemlib1:iemlib2:iemgui:iemmatrix:iem_ambi"
 
 ${PD} $PDPATHS $PDLIBS $@
