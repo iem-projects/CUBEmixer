bb6adb9d7995d7b98a636c989a4ee0d05bba38f6
IOhannes m zmölnig <zmoelnig@iem.at>
2005-06-29 18:26:38 +0000

    added abs-path

--- lib/GUI/start.sh
+++ lib/GUI/start.sh
@@ -5,7 +5,7 @@ then
   PD=pd
 fi
 
-PDPATHS="-path .:..:../abs/:../libs/:../plugins/:abs"
+PDPATHS="-path .:..:../abs/:../libs/:../plugins/:abs:../libs/zexyabs/:../libs/iemabs"
 PDLIBS="-lib zexy:iemlib1:iemlib2:iemgui:iemmatrix"
 
 ${PD} $PDPATHS $PDLIBS $@
