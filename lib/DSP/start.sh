e0afb66c5e0b24b4b22db348e3e6120bd24b1094
IOhannes m zmölnig <zmoelnig@iem.at>
2005-06-30 14:11:33 +0000

    "-defaults" flag for some defaults

--- lib/DSP/start.sh
+++ lib/DSP/start.sh
@@ -5,7 +5,17 @@ then
   PD=pd
 fi
 
+echo argc $#
+ARGS=$@
+if [ "$#" != "0" ]; then
+ if [ "x$1" = "x-defaults" ]; then
+  shift
+  ARGS="-rt -channels 24 $@"
+ fi
+fi
+
 PDPATHS="-path .:..:../abs/:../libs/iemabs:../libs/zexyabs:../libs/:../plugins/:abs"
 PDLIBS="-lib zexy:iemlib1:iemlib2:iemgui:iemmatrix:iem_ambi"
 
-${PD} $PDPATHS $PDLIBS $@
+${PD} ${PDPATHS} ${PDLIBS} ${ARGS}
+
