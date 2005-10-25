e3f9dbb4db4e99c80bc6ab434ccde837145f58c5
IOhannes m zmölnig <zmoelnig@iem.at>
2005-10-25 15:50:47 +0000

    added "-mididev" flag in order to make alsamidi work out of the box

--- lib/startALL.sh
+++ lib/startALL.sh
@@ -35,7 +35,7 @@ fi
 PDPATHS="-path .:abs/:libs/:plugins/:GUI/:DSP/:GUI/abs:DSP/abs:libs/iemabs:libs/zexyabs"
 PDLIBS="-lib zexy:iemlib1:iemlib2:iemgui:iemmatrix:iem_ambi"
 
-PDDSPFLAGS="-32bit -rt -channels 24 -blocksize 16 -audiobuf 150 -alsamidi"
+PDDSPFLAGS="-32bit -rt -channels 24 -blocksize 16 -audiobuf 50 -alsamidi -mididev 1"
 PDGUIFLAGS="-nosound -nomidi"
 
 ${PD} ${PDPATHS} ${PDLIBS} ${PDDSPFLAGS} DSP/DSP+NET+MIDI+CUE.pd &
