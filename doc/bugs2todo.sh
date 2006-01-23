0f8fabeaa7f8743aa51b8b8c2231e2e16bf53948
IOhannes m zmölnig <zmoelnig@iem.at>
2006-01-23 11:59:38 +0000

    added header for TODO.txt

--- doc/bugs2todo.sh
+++ doc/bugs2todo.sh
@@ -3,6 +3,17 @@
 BUGS=BUGS.txt
 TODO=TODO.txt
 
+
+function print_header(){
+echo "ACHTUNG: bitte in diesem file nichts editieren!"
+echo "ACHTUNG: statt dessen das file ${BUGS} editieren!"
+echo "ACHTUNG:"
+echo "ACHTUNG: (dieses file ist nur ein "view" auf ${BUGS})"
+echo 
+}
+
+print_header > ${TODO}
+
 cat ${BUGS} | \
  egrep "^([[:alnum:]]|([[:space:]]*\-))" | \
  sed "/^[[:alnum:]]/{x;p;x}" \
