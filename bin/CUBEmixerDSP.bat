4f0c320279f1bd657ba9846239efe2e1605c4ee7
IOhannes m zmölnig <zmoelnig@iem.at>
2006-03-09 12:57:18 +0000

    filled in default values into LocalVars.template.bat (which used to be called LocalVars.bat.template); CUBEmixer*.bat now include both LocalVars.template.bat and (if it exists) LocalVars.bat to get all the variables

--- bin/CUBEmixerDSP.bat
+++ bin/CUBEmixerDSP.bat
@@ -1,18 +1,13 @@
 @echo off
 
+set LOCALVARSTEMPLATE=../etc/CUBEmixer/LocalVars.template.bat
 set LOCALVARS=../etc/CUBEmixer/LocalVars.bat
 
-set PD_INSTALL=..\src\pd\bin
-set PD_OPTIONS=-font 10
-set PD_PATH=-path ..;../lib;../lib/abs/;../lib/libs/;../lib/plugins/;../lib/extensions/;../lib/GUI/;../lib/DSP/;../lib/GUI/abs;../lib/DSP/abs;../lib/libs/iemabs;../lib/libs/zexyabs
-set PD_LIB=-lib zexy;iemlib1;iemlib2;iemmatrix;iem_ambi;iemgui
+REM default values
+if exist %LOCALVARSTEMPLATE% (call $LOCALVARSTEMPLATE%)
 
-set DSP_PATCH=..\lib\DSP\DSP+NET+MIDI+CUE_WIN.pd
-set DSP_AUDIO=-r 44100 -audiobuf 80 -sleepgrain 10 -channels 26
-set DSP_MIDI=
-
-REM ***** overwrites optionally the PD_INSTALL variable *********
+REM ***** overrides optionally the PD_INSTALL variable *********
 if exist %LOCALVARS% (call $LOCALVARS%)
 
-@echo starting pd ...
+echo starting pd ...
 %PD_INSTALL%\pd %DSP_AUDIO% %DSP_MIDI% %DSP_OPTIONS% %PD_OPTIONS% %DSP_PATH% %PD_PATH% %DSP_LIB %PD_LIB% %DSP_PATCH%
--- bin/CUBEmixerGUI.bat
+++ bin/CUBEmixerGUI.bat
@@ -1,19 +1,14 @@
 @echo off
 
+
+set LOCALVARSTEMPLATE=../etc/CUBEmixer/LocalVars.template.bat
 set LOCALVARS=../etc/CUBEmixer/LocalVars.bat
 
-set PD_INSTALL=..\src\pd\bin
-set PD_OPTIONS=-font 10
-set PD_PATH=-path ..;../lib;../lib/abs/;../lib/libs/;../lib/plugins/;../lib/extensions/;../lib/GUI/;../lib/DSP/;../lib/GUI/abs;../lib/DSP/abs;../lib/libs/iemabs;../lib/libs/zexyabs
-set PD_LIB=-lib zexy;iemlib1;iemlib2;iemmatrix;iem_ambi;iemgui
+REM default values
+if exist %LOCALVARSTEMPLATE% (call $LOCALVARSTEMPLATE%)
 
-set GUI_PATCH=../lib/GUI/GUI16+OUT.pd
-set GUI_AUDIO=-noaudio
-set GUI_MIDI=-nomidi
-
-
-REM ***** overwrites optionally the PD_INSTALL variable *********
-if exist LocalVars.bat (call LocalVars.bat)
+REM ***** overrides optionally the PD_INSTALL variable *********
+if exist %LOCALVARS% (call $LOCALVARS%)
 
 @echo starting pd ...
 %PD_INSTALL%\pd %GUI_AUDIO% %GUI_MIDI% %GUI_OPTIONS% %PD_OPTIONS% %GUI_PATH% %PD_PATH% %GUI_LIB% %PD_LIB% %GUI_PATCH%
