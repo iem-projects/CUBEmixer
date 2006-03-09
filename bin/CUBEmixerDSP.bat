04e02809cb5fc7a4c9efc3f94b92ecea14453a65
IOhannes m zmölnig <zmoelnig@iem.at>
2006-03-09 09:58:58 +0000

    moved LocalVars.bat from ./bin to ./etc/CUBEmixer/; added the CUBEmixer*.bat files accordingly

--- bin/CUBEmixerDSP.bat
+++ bin/CUBEmixerDSP.bat
@@ -1,15 +1,18 @@
 @echo off
 
-set PD_PATCH=..\lib\DSP\DSP+NET+MIDI+CUE_WIN.pd
+set LOCALVARS=../etc/CUBEmixer/LocalVars.bat
+
 set PD_INSTALL=..\src\pd\bin
-set PD_AUDIO=-r 44100 -audiobuf 80 -sleepgrain 10 -channels 26
-set PD_MIDI=
 set PD_OPTIONS=-font 10
 set PD_PATH=-path ..;../lib;../lib/abs/;../lib/libs/;../lib/plugins/;../lib/extensions/;../lib/GUI/;../lib/DSP/;../lib/GUI/abs;../lib/DSP/abs;../lib/libs/iemabs;../lib/libs/zexyabs
 set PD_LIB=-lib zexy;iemlib1;iemlib2;iemmatrix;iem_ambi;iemgui
 
+set DSP_PATCH=..\lib\DSP\DSP+NET+MIDI+CUE_WIN.pd
+set DSP_AUDIO=-r 44100 -audiobuf 80 -sleepgrain 10 -channels 26
+set DSP_MIDI=
+
 REM ***** overwrites optionally the PD_INSTALL variable *********
-if exist LocalVars.bat (call LocalVars.bat)
+if exist %LOCALVARS% (call $LOCALVARS%)
 
 @echo starting pd ...
-%PD_INSTALL%\pd %PD_AUDIO% %PD_MIDI% %PD_OPTIONS% %PD_PATH% %PD_LIB% %PD_PATCH%
+%PD_INSTALL%\pd %DSP_AUDIO% %DSP_MIDI% %DSP_OPTIONS% %PD_OPTIONS% %DSP_PATH% %PD_PATH% %DSP_LIB %PD_LIB% %DSP_PATCH%
