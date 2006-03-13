b4cf5a1a2eb76b4709629ee31b34066dd10bb61d
IOhannes m zmölnig <zmoelnig@iem.at>
2006-03-13 10:17:17 +0000

    fixed typo (on w32 variables are %VAR% and not $VAR or even $VAR%)

--- bin/CUBEmixerDSP.bat
+++ bin/CUBEmixerDSP.bat
@@ -4,10 +4,10 @@ set LOCALVARSTEMPLATE=../etc/CUBEmixer/LocalVars.template.bat
 set LOCALVARS=../etc/CUBEmixer/LocalVars.bat
 
 REM default values
-if exist %LOCALVARSTEMPLATE% (call $LOCALVARSTEMPLATE%)
+if exist %LOCALVARSTEMPLATE% (call %LOCALVARSTEMPLATE%)
 
 REM ***** overrides optionally the PD_INSTALL variable *********
-if exist %LOCALVARS% (call $LOCALVARS%)
+if exist %LOCALVARS% (call %LOCALVARS%)
 
 echo starting pd ...
 %PD_INSTALL%\pd %DSP_AUDIO% %DSP_MIDI% %DSP_OPTIONS% %PD_OPTIONS% %DSP_PATH% %PD_PATH% %DSP_LIB %PD_LIB% %DSP_PATCH%
--- bin/CUBEmixerGUI.bat
+++ bin/CUBEmixerGUI.bat
@@ -5,10 +5,10 @@ set LOCALVARSTEMPLATE=../etc/CUBEmixer/LocalVars.template.bat
 set LOCALVARS=../etc/CUBEmixer/LocalVars.bat
 
 REM default values
-if exist %LOCALVARSTEMPLATE% (call $LOCALVARSTEMPLATE%)
+if exist %LOCALVARSTEMPLATE% (call %LOCALVARSTEMPLATE%)
 
 REM ***** overrides optionally the PD_INSTALL variable *********
-if exist %LOCALVARS% (call $LOCALVARS%)
+if exist %LOCALVARS% (call %LOCALVARS%)
 
-@echo starting pd ...
+echo starting pd ...
 %PD_INSTALL%\pd %GUI_AUDIO% %GUI_MIDI% %GUI_OPTIONS% %PD_OPTIONS% %GUI_PATH% %PD_PATH% %GUI_LIB% %PD_LIB% %GUI_PATCH%
