a56db0e7e0a1da3491fc84b244cd29928497bb49
Winfried Ritsch <ritsch@iem.at>
2006-04-03 08:54:38 +0000

    now can take arguments for a DSP and a GUI patch

--- bin/CUBEmixer.sh
+++ bin/CUBEmixer.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-
-
 ## this function returns the first of its arguments that is
 ## an executable file
 ## if no executable files are given, it returns the default
@@ -50,12 +48,25 @@ if [ "x" != "x${GUI_PATCH}" ]; then
  GUI_PATCH="-open ${GUI_PATCH}"
 fi
 
+if [ "x" != "x${2}" ]; then
+ if [ -f $2 ]; then
+    GUI_EXTPATCH="-open $2"
+ fi
+fi
+
+
 if [ "x" != "x${DSP_PATCH}" ]; then
  DSP_PATCH="-open ${DSP_PATCH}"
 fi
 
-${PD} ${GUI_AUDIO} ${GUI_MIDI} ${GUI_OPTIONS} ${PD_OPTIONS} ${GUI_PATH} ${PD_PATH} ${GUI_LIB} ${PD_LIB} ${GUI_PATCH} \
+if [ "x" != "x${1}" ]; then
+ if [ -f $1 ]; then
+    DSP_EXTPATCH="-open $1"
+ fi
+fi
+
+${PD} ${GUI_AUDIO} ${GUI_MIDI} ${GUI_OPTIONS} ${PD_OPTIONS} ${GUI_PATH} ${PD_PATH} ${GUI_LIB} ${PD_LIB} ${GUI_PATCH} ${GUI_EXTPATCH}\
       -send "${GUI_MESSAGE}" -send "${PD_MESSAGE}" &
 
-${PD} ${DSP_AUDIO} ${DSP_MIDI} ${DSP_OPTIONS} ${PD_OPTIONS} ${DSP_PATH} ${PD_PATH} ${DSP_LIB} ${PD_LIB} ${DSP_PATCH} \
+${PD} ${DSP_AUDIO} ${DSP_MIDI} ${DSP_OPTIONS} ${PD_OPTIONS} ${DSP_PATH} ${PD_PATH} ${DSP_LIB} ${PD_LIB} ${DSP_PATCH} ${DSP_EXTPATCH} \
       -send "${DSP_MESSAGE}" -send "${PD_MESSAGE}"
