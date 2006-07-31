c38236a1b8f0dcfd4f988157915255dc8f2d3caf
IOhannes m zmölnig <zmoelnig@iem.at>
2006-07-31 16:10:57 +0000

    put the comment for the test_pd() functionwithin the function-body (better readability)

--- bin/CUBEmixer.sh
+++ bin/CUBEmixer.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
+#ECHO=echo
+
+function test_pd() {
 ## this function returns the first of its arguments that is
 ## an executable file
 ## if no executable files are given, it returns the default
 ## pd-binary (if any)
 ## usage:
 ##  test_pd /path/to/my/preferred/pd /path/to/my/second/choice/pd
-
-#ECHO=echo
-
-function test_pd() {
 local i
 local ARGS
 ARGS="$@ $(which pd)"
