22f6e6fc86ecf105a64c333f83bb0ce2a381facf
IOhannes m zmölnig <zmoelnig@iem.at>
2009-05-10 15:10:27 +0000

    start.sh now accepts arguments

--- tools/ambidecalc/start.sh
+++ tools/ambidecalc/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pd -path ../../lib/libs:../../lib/DSP/abs -lib iemmatrix:zexy:iem_ambi2:iemgui:iemlib1:iemlib2 ambidecigner.pd
+pd -path ../../lib/libs:../../lib/DSP/abs -lib iemmatrix:zexy:iem_ambi2:iemgui:iemlib1:iemlib2 -open ambidecigner.pd $@
