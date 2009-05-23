56bb82fa74ec2c13259d87eff770578f627b8a63
Winfried Ritsch <ritsch@iem.at>
2009-05-23 20:04:11 +0000

    added new start script for ambidecalc under linux

---
+++ bin/Ambidecalc.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#ECHO=echo
+
+CUBEMIXERPATH=$(dirname $0)/..
+
+# source functions
+. ${CUBEMIXERPATH}/lib/functions.sh
+
+cd ${CUBEMIXERPATH}/tools/ambidecalc/ || (echo "directory for AMBIDECALC not found!!"; exit -1)
+
+LOCALVARSTEMPLATE="etc/CUBEmixer/LocalVars.template.sh"
+LOCALVARS="etc/CUBEmixer/LocalVars.sh"
+
+if [ -e ${LOCALVARSTEMPLATE} ]; then
+ . ${LOCALVARSTEMPLATE}
+fi
+if [ -e ${LOCALVARS} ]; then
+ . ${LOCALVARS}
+fi
+
+PD=$(test_pd $PD ${PD_INSTALL}/pd)
+
+if [ "x$PD" = "x" ]
+then
+  echo "pd not found!"
+  echo "set the environment variable PD to the full-path to the pd binary"
+  echo "e.g.: \"export PD=/usr/bin/pd\""
+  echo
+  exit -1
+fi
+
+./AMBIDECALC.sh
