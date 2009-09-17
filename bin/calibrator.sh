8e480535edb5e0072e15e8f668b8fe50b161649d
Winfried Ritsch <ritsch@iem.at>
2009-09-17 11:24:08 +0000

    corrected the pathes and included LocalVars to the calibrate patch

--- bin/calibrator.sh
+++ bin/calibrator.sh
@@ -9,16 +9,4 @@ CUBEMIXERPATH=$(dirname $0)/..
 
 cd ${CUBEMIXERPATH}/tools/calibrator/ || (echo "directory for CALIBRATOR not found!!"; exit -1)
 
-LOCALVARSTEMPLATE="etc/CUBEmixer/LocalVars.template.sh"
-LOCALVARS="etc/CUBEmixer/LocalVars.sh"
-
-if [ -e ${LOCALVARSTEMPLATE} ]; then
- . ${LOCALVARSTEMPLATE}
-fi
-if [ -e ${LOCALVARS} ]; then
- . ${LOCALVARS}
-fi
-
-PD=$(test_pd $PD ${PD_INSTALL}/pd)
-
 ./calibrate.sh
