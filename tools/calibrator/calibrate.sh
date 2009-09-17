c0951b8093ffe496dc60209dee4dbe180eb1a116
Winfried Ritsch <ritsch@iem.at>
2009-09-17 11:23:45 +0000

    corrected the pathes and included LocalVars to the calibrate patch

--- tools/calibrator/calibrate.sh
+++ tools/calibrator/calibrate.sh
@@ -2,14 +2,29 @@
 
 cd ${0%/*}
 
-CALIBRATOR_PATCH="CALIBRATOR.pd"
-CALIBRATOR_PATH="-path ../../lib/libs/"
-CALIBRATOR_LIBS="-lib zexy:iemmatrix"
-
 . ../../lib/functions.sh
 
-PDINSTANCE=../../lib/pd/bin/pd
+CUBEMIXERPATH=$(dirname $0)/../..
 
-PD=$(test_pd $PD $PDINSTANCE)
+CALIBRATOR_PATCH="CALIBRATOR.pd"
+CALIBRATOR_PATH="-path ${CUBEMIXERPATH}/lib/libs/"
+CALIBRATOR_LIBS="-lib zexy:iemmatrix"
 
-$PD -rt -inchannels 2 ${CALIBRATOR_PATH} ${CALIBRATOR_LIBS} ${CALIBRATOR_PATCH}
+
+PDINSTANCE=${CUBEMIXERPATH}/lib/pd/bin/pd
+
+
+
+LOCALVARSTEMPLATE="${CUBEMIXERPATH}/etc/CUBEmixer/LocalVars.template.sh"
+LOCALVARS="${CUBEMIXERPATH}/etc/CUBEmixer/LocalVars.sh"
+
+if [ -e ${LOCALVARSTEMPLATE} ]; then
+ . ${LOCALVARSTEMPLATE}
+fi
+if [ -e ${LOCALVARS} ]; then
+ . ${LOCALVARS}
+fi
+
+PD=$(test_pd  ${PDINSTANCE} ${PD})
+
+$ECHO $PD ${DSP_OPTIONS} ${DSP_AUDIO}  ${CALIBRATOR_PATH} ${CALIBRATOR_LIBS} ${CALIBRATOR_PATCH}
