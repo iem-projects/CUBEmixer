dbc8e69b5277f5ac0760ac1beed1513f34d58f02
Winfried Ritsch <ritsch@iem.at>
2009-09-29 12:03:02 +0000

    added now real outs to calibrator, a la long should migrate to an extension in CUBEMIXER

--- tools/calibrator/calibrate.sh
+++ tools/calibrator/calibrate.sh
@@ -10,10 +10,26 @@ CALIBRATOR_PATCH="CALIBRATOR.pd"
 CALIBRATOR_PATH="-path ${CUBEMIXERPATH}/lib/libs/"
 CALIBRATOR_LIBS="-lib zexy:iemmatrix"
 
+ETC=etc/CUBEmixer
+ETC_PATH="-path ${ETC}"
+
+LOCALVARSTEMPLATE="${ETC}/LocalVars.template.sh"
+STARTUPPATCHES="${ETC}/configured.sh"
+LOCALVARS="${LOCALETC}/LocalVars.sh" 
+
+PD_INSTALL="lib/pd/bin"
+
+if [ -e ${LOCALVARSTEMPLATE} ]; then
+ . ${LOCALVARSTEMPLATE}             
+fi                                  
+if [ -e ${STARTUPPATCHES} ]; then   
+ . ${STARTUPPATCHES}                
+fi                                  
+if [ -e ${LOCALVARS} ]; then        
+ . ${LOCALVARS}                     
+fi
 
 PDINSTANCE=${CUBEMIXERPATH}/lib/pd/bin/pd
-
-
 
 LOCALVARSTEMPLATE="${CUBEMIXERPATH}/etc/CUBEmixer/LocalVars.template.sh"
 LOCALVARS="${CUBEMIXERPATH}/etc/CUBEmixer/LocalVars.sh"
@@ -27,4 +43,6 @@ fi
 
 PD=$(test_pd  ${PDINSTANCE} ${PD})
 
-$ECHO $PD ${DSP_OPTIONS} ${DSP_AUDIO}  ${CALIBRATOR_PATH} ${CALIBRATOR_LIBS} ${CALIBRATOR_PATCH}
+$ECHO $PD ${DSP_OPTIONS} ${DSP_AUDIO}  ${CALIBRATOR_PATH} ${CALIBRATOR_LIBS} \
+ ${DSP_OPTIONS} ${LOCALETC_PATH} ${ETC_PATH} ${DSP_PATH} ${PD_PATH} ${PD_HELPPATH} ${DSP_LIB} ${PD_LIB}\
+ ${CALIBRATOR_PATCH}
